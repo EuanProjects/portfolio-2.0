da9696425ef94ae1c8dc7a2175c49c444b1ce333
ejcanoy <euan.james.canoy@gmail.com>
2023-09-18 14:23:11 -0700

    resized project section

--- src/projectsSection/projectsSection.jsx
+++ src/projectsSection/projectsSection.jsx
@@ -3,9 +3,10 @@ import projectData from "./project/projectData";
 
 function ProjectsSection() {
     const viewHeight = projectData.length * 50 + "vh";
+    const smallerViewHeight = projectData.length * 10 + "vh";
     return (
         <>
-            <div className={`h-[${viewHeight}] mb-48 flex justify-center sm:px-6 xs:px-6 `} id="projects">
+            <div className={`md:h-[${viewHeight}] sm:h-[${smallerViewHeight}] xs:h-[${smallerViewHeight}] mb-48 flex justify-center sm:px-6 xs:px-6 `} id="projects">
                 <div className="md:w-[80vw] lg:w-[73vw] 2xl:w-1/2">
                     <div className="flex items-center">
                         <h2 className="sm:text-4xl xs:text-2xl">
