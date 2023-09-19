b8412606285156da1a5035efb86a016b6ef937fc
ejcanoy <euan.james.canoy@gmail.com>
2023-09-19 12:17:47 -0700

    added nav styling

--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react"
 import { Link } from "react-scroll"
 import { Menu } from "react-feather";
 import NavMenu from "./navMenu";
+import './styles/navbar.css';
+
 
 function Navbar({ handleClick, displayMenu }) {
+    const [activeSection, setActiveSection] = useState(null);
 
     return (
         <>
@@ -13,17 +17,17 @@ function Navbar({ handleClick, displayMenu }) {
                             <a className="text-xl">EC</a>
                         </div>
                         <div className="space-x-5 lg:block sm:hidden xs:hidden">
-                            <button>
-                                <Link to="hero" spy={true} smooth={true} offset={-50} duration={500} ><span className="text-green text-xs font-mono">01. </span>Home</Link>
+                            <button className={`w-[100px] parent ${activeSection === 'hero' ? 'active' : ''}`}>
+                                <Link to="hero" spy={true} smooth={true} offset={-50} duration={500} onSetActive={() => setActiveSection('hero')} activeClass=""><span className="text-green text-xs font-mono">01. </span>Home</Link>
                             </button>
-                            <button>
-                                <Link to="about" spy={true} smooth={true} offset={25} duration={500} ><span className="text-green text-xs font-mono">01. </span>About</Link>
+                            <button className={`w-[100px] parent ${activeSection === 'about' ? 'active' : ''}`}>
+                                <Link to="about" spy={true} smooth={true} offset={25} duration={500} onSetActive={() => setActiveSection('about')} activeClass=""><span className="text-green text-xs font-mono">01. </span>About</Link>
                             </button>
-                            <button>
-                                <Link to="projects" spy={true} smooth={true} offset={-75} duration={500} ><span className="text-green text-xs font-mono">01. </span>Projects</Link>
+                            <button className={`w-[100px] parent ${activeSection === 'projects' ? 'active' : ''}`}>
+                                <Link to="projects" spy={true} smooth={true} offset={-75} duration={500} onSetActive={() => setActiveSection('projects')} activeClass=""><span className="text-green text-xs font-mono">01. </span>Projects</Link>
                             </button>
-                            <button>
-                                <Link to="contact" spy={true} smooth={true} offset={-50} duration={500} ><span className="text-green text-xs font-mono">01. </span>Contact</Link>
+                            <button className={`w-[100px] parent ${activeSection === 'contact' ? 'active' : ''}`}>
+                                <Link to="contact" spy={true} smooth={true} offset={-50} duration={500} onSetActive={() => setActiveSection('contact')} activeClass=""><span className="text-green text-xs font-mono">01. </span>Contact</Link>
                             </button>
                         </div>
                         <div className="lg:hidden sm:block">
